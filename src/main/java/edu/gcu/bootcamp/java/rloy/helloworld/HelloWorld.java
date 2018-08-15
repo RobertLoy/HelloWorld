4e7d9360dcdcbcd269b4cb240344012741bf1bd1
Robert Loy <robert.loy@gcu.edu>
2018-08-15 15:09:17 -0700

    Testing  update

--- src/main/java/edu/gcu/bootcamp/java/rloy/helloworld/HelloWorld.java
+++ src/main/java/edu/gcu/bootcamp/java/rloy/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package edu.gcu.bootcamp.java.rloy.helloworld;
 public class HelloWorld {
 	
 	public static void main ( String [] args) {
-		System.out.println("Hello Lopes");
+		System.out.println("Hello Lopes!");
 	}
 
 }
